75df6a4acb3aca655cdc471ae5c235f260dcc9d5
alexradu04 <alex.radu04@yahoo.com>
2020-06-07 12:47:04 +0300

    Added help command

--- index.js
+++ index.js
@@ -153,6 +153,9 @@ function onMessageHandler(target, context, msg, self) {
     if(msg==='!github') {
         client.say(target, `Check out my spaghetti here: https://github.com/alexradu04/Tsu-Queue-Manager-Bot`);
     }
+    if(msg==='!help') {
+        client.say(target, "List of commands: https://imgur.com/HhKO9wc")
+    }
 }
 
 client.connect();
